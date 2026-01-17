8a7619bf8af827f8f876ccb05ce8e3dbc88d72cc
jack4491 <jackwillosmond@Outlook.com>
2026-01-17 12:13:29 -0500

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WhenFree
+
 A real-time scheduling app that emphasizes human interaction through group availability and event coordination making group planning effortless. This was built for the Jan 2026 United Hackathon \\ V6.
---
+++ app/frontend/src/util/types.ts
@@ -0,0 +1,15 @@
+export type EntryType = "availability" | "event";
+
+export interface CalendarEntry {
+    id: string;
+    name: string;
+    title: string;
+    type: EntryType;
+    startTime: string; // ISO string
+    endTime: string;   // ISO string
+}
+
+export interface Lobby {
+    name: string;
+    code: string;
+}
